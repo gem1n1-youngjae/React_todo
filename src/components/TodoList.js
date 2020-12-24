ce897e22b7354f79c1131c652ee8416ccbde3500
pdlma0904 <57673815+pdlma0904@users.noreply.github.com>
2020-12-24 22:07:11 +0900

    [Update] TodoObject Insert Delete

---
+++ src/components/TodoList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import TodoListItem from "./TodoListItem";
+
+const TodoList = ({ todos, onRemove }) => {
+  return (
+    <div className="TodoList">
+      {todos.map((todo) => (
+        <TodoListItem todo={todo} key={todo.id} onRemove={onRemove}/>
+      ))}
+    </div>
+  );
+};
+
+export default TodoList;
